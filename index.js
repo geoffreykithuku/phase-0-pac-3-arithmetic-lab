7987c8446a4cf015aff2b8e48a824625120bbf25
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-08-01 10:57:13 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(){
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a,b){
+
+}
+function subtract(a,b){
+
+}
+function preserveDecimal(){
+
+}
+function makeInt(){
+    
+}
